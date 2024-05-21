d8ce3d4684f9cb9b5a2851debb46ab780e23ce04
jhydh666666 <duanhong.yang.markus@gmail.com>
2024-05-21 18:21:29 +0800

    Update IKK2.py

--- IKK2.py
+++ IKK2.py
@@ -4,20 +4,31 @@ import matplotlib.pyplot as plt  # type: ignore
 L1 = 100 
 L2 = 100
 L3 = 100
+
 def calculate(Rz, Px, Py):
     Wx = Px - L3 * np.cos(Rz)
     Wy = Py - L3 * np.sin(Rz) 
+
     L4 = np.sqrt(Wx**2 + Wy**2)
+
     th2_new = np.arccos((Wx**2 + Wy**2 - L1**2 - L2**2) / (2 * L1 * L2))
+
     fai = np.arctan2(Wy, Wx)
     x = L2 * np.cos(th2_new)
     a = np.arcsin(x / L4)
+
     th1_new = fai - a
+
     th3_new = Rz - th2_new - th1_new  
 
     th1_deg = np.rad2deg(th1_new)
     th2_deg = np.rad2deg(th2_new)
     th3_deg = np.rad2deg(th3_new) 
+
+# 限制th1, th2, th3 的角度 
+    th1_deg = np.clip(th1_deg, 0 , 150)
+    th2_deg = np.clip(th2_deg, 100, 100)
+    th3_deg = np.clip(th3_deg, -100, 100)
 
     return th1_deg, th2_deg, th3_deg
 
@@ -39,8 +50,8 @@ def plot(L1, L2, L3, th1_deg, th2_deg, th3_deg):
     y3 = y2 + L3 * np.sin(th1 + th2 + th3)
 
     plt.plot([x0, x1], [y0, y1], label=f'L1 = {L1}, θ1 = {np.rad2deg(th1):.2f}°')
-    plt.plot([x1, x2], [y1, y2], label=f'L2 = {L2}, θ2 = {np.rad2deg(th1 + th2):.2f}°')
-    plt.plot([x2, x3], [y2, y3], label=f'L3 = {L3}, θ3 = {np.rad2deg(th1 + th2 + th3):.2f}°')
+    plt.plot([x1, x2], [y1, y2], label=f'L2 = {L2}, θ1+θ2 = {np.rad2deg(th1 + th2):.2f}°')
+    plt.plot([x2, x3], [y2, y3], label=f'L3 = {L3}, θ1+θ2+θ3 = {np.rad2deg(th1 + th2 + th3):.2f}°')
 
     plt.scatter([x0, x1, x2, x3], [y0, y1, y2, y3], color='red')  # 绘制点
     plt.title('机械臂的运动学表示')
@@ -54,10 +65,14 @@ def plot(L1, L2, L3, th1_deg, th2_deg, th3_deg):
     # 显示图像
     plt.show()
 
+# Rz_deg = float(input("Please enter the value of Rz_deg"))
 Rz_deg =100
 Rz = np.deg2rad(Rz_deg)
-Px = 120
-Py = 100
+# Px = float(input('Please enter the value of Px'))
+Px = 150 
+# Py = float(input('Please enter the value of Py'))
+Py = 150 
+
 
 th1_deg, th2_deg, th3_deg = calculate(Rz, Px, Py)
 
